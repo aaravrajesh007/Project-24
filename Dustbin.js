37602efa9e9cb50fcb25ae044ca3bd453cbeeb20
aaravrajesh007 <72609492+aaravrajesh007@users.noreply.github.com>
2020-12-10 08:48:01 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,24 @@
+class Dustbin{
+    constructor(x,y){
+var options={
+    isStatic:true
+}
+this.width=200
+this.height=100
+this.thickness=20
+this.bwall=Bodies.rectangle(x,y,this.width,this.thickness,options)
+this.lwall=Bodies.rectangle(x-this.width/2,y-this.height/2,this.thickness,this.height,options)
+this.rwall=Bodies.rectangle(x+this.width/2,y-this.height/2,this.thickness,this.height,options)
+World.add(world,this.bwall);
+World.add(world,this.rwall);
+World.add(world,this.lwall);
+    }
+display(){
+    noStroke();
+    fill("orange");
+  rect(this.bwall.position.x,this.bwall.position.y,this.width,this.thickness);
+  rect(this.lwall.position.x,this.lwall.position.y,this.thickness,this.height);
+  rect(this.rwall.position.x,this.rwall.position.y,this.thickness,this.height);  
+  
+}
+}
